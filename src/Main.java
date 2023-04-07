fa722a5b92e0d22b08589c211e0ed729f5946d10
Haim5 <89258839+Haim5@users.noreply.github.com>
2023-04-07 05:14:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        Manager manager = new Manager(0.5, 2);
-        manager.Run();
+        Facade c = new Facade();
+        c.play();
     }
 }
