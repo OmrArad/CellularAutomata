e25cdb4a2f218c64ff4ed64930c03891e9b834bd
OmrArad <omerarad2@gmail.com>
2023-04-21 14:07:08 +0300

    S count in Elliptical

--- src/EllipticalSolution.java
+++ src/EllipticalSolution.java
@@ -10,6 +10,8 @@ public class EllipticalSolution extends Solution{
      */
     public EllipticalSolution(int gridSize, int l) {
         this.l = l;
+        int[] s_counters = new int[DOUBT_LEVELS];
+        // color the grid
         for (int i = 0; i < gridSize / 2; i++) {
             for (int j = 0; j < 4; j++) {
                 int cx = gridSize / 2;
@@ -24,6 +26,7 @@ public class EllipticalSolution extends Solution{
 
                             Location loc = new Location(x, y);
                             Person p = new Person(loc, j + 1, l, gridSize);
+                            s_counters[j]++;
                             this.m.put(loc, p);
                         }
                     }
@@ -32,5 +35,15 @@ public class EllipticalSolution extends Solution{
         }
         // set first
         this.setFirstRandomly();
+
+        // set S-values
+        int sum = 0;
+        for (int d : s_counters) {
+            sum += d;
+        }
+        this.s1 = (double) s_counters[0]/sum;
+        this.s2 = (double) s_counters[1]/sum;
+        this.s3 = (double) s_counters[2]/sum;
+        this.s4 = (double) s_counters[3]/sum;
     }
 }
